82ed1548cb0aaafecf867fb3a3f5fd7d64a475f8
Evgeniy <snezkoevgen@yandex.ru>
2021-04-08 23:43:58 +0300

    third

--- MyPlacesApp/MainViewController.swift
+++ MyPlacesApp/MainViewController.swift
@@ -28,6 +28,8 @@ class MainViewController: UITableViewController {
      let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
 
 
         return cell
